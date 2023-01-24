49c4bed6228b630b994309ad902371649495600a
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-01-24 15:44:36 -0600

    JSON server delete works

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -17,6 +17,11 @@ export class TasksComponent implements OnInit {
   ngOnInit(): void {
     this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks);
   }
+
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => this.tasks = this.tasks.filter(t => t.id !== task.id));
+    this.tasks = this.tasks.filter(t => t.id !== task.id)
+  }
          
 
 }
